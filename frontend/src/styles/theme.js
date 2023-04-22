1e484e706c7b0f9856f635f344c2b18af469e4a0
David Wang <david.wang5766@gmail.com>
2023-04-22 12:54:36 -0700

    resolving merge conflicts

---
+++ frontend/src/styles/theme.js
@@ -0,0 +1,10 @@
+import {createTheme} from '@mui/material/styles';
+
+const theme = createTheme({
+    colors: {
+        primary: '#66B088',
+        secondary: '#B4D7C4'
+    }
+});
+
+export default theme
