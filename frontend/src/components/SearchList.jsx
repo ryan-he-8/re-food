51b141925e9fcb7f66ed60acbc75a1c69c3118f3
David Wang <david.wang5766@gmail.com>
2023-04-23 01:20:43 -0700

    added limit to items for search list

--- frontend/src/components/SearchList.jsx
+++ frontend/src/components/SearchList.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { IconButton, List, ListItem } from '@mui/material';
+import { IconButton, List, ListItem, Box } from '@mui/material';
 import { AddBoxOutlined } from '@mui/icons-material';
 export default function SearchList ({searchQuery, ingredients, addIngredient}) {
     function matchQuery(searchQuery, ingredientName){
@@ -7,11 +7,15 @@ export default function SearchList ({searchQuery, ingredients, addIngredient}) {
         return ingredientName.toLowerCase().includes(searchQuery.toLowerCase());
     }
     return (
-        <List>
+        <Box
+        overflow={'scroll'}
+        height={'60vh'}>
+        <List
+        >
             {ingredients
             .filter((ingredient) => ingredient.quantity === 0)
             .filter((ingredient) => matchQuery(searchQuery, ingredient.name))
-            .map((ingredient) => (
+            .map((ingredient, index) => (
                 <ListItem
                 key={ingredient.name}
                 secondaryAction = {
@@ -22,7 +26,8 @@ export default function SearchList ({searchQuery, ingredients, addIngredient}) {
                 }>
                     {ingredient.name}
                 </ListItem>
-            ))}
+            )).slice(0, 5)}
         </List>
+        </Box>
     )
 }
